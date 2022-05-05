9e090b626c8da475449bde5a3bb32cb63d9b5214
kurniadikevin <kurniadikevin@gmail.com>
2022-05-05 20:37:31 +0700

    first progress

---
+++ script.js
@@ -0,0 +1,10 @@
+let container = document.querySelector('.container');
+
+ for (let i = 0; i < 256; i++){
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+    container.appendChild(grid);
+}
+
+
+
