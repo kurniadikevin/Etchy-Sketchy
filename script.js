9cfacfa677158b0c9dc6a7f9af13ab6f8ebcf55b
kurniadikevin <kurniadikevin@gmail.com>
2022-05-07 16:20:38 +0700

    resize done colur undo, need max 100 by 100

--- script.js
+++ script.js
@@ -1,10 +1,44 @@
-let container = document.querySelector('.container');
+// make grid and eventListener
+function makeGrid(input){
+        let container = document.querySelector('.container');
+        container.style.gridTemplateColumns =   `repeat( ${input}, auto)`
+        container.style.gridTemplateRows =   `repeat( ${input}, auto)`
+        for (let i = 0; i < input * input; i++){
+            const grid = document.createElement('div');
+            grid.classList.add('grid');
+            container.appendChild(grid);
+        }
 
- for (let i = 0; i < 256; i++){
-    const grid = document.createElement('div');
-    grid.classList.add('grid');
-    container.appendChild(grid);
+        const gridAll = document.querySelectorAll ('.grid');
+
+        for (i=0; i < gridAll.length; i++){ 
+            gridAll[i].addEventListener('mouseover',function(object){
+                object.target.style.backgroundColor = "black";
+            })
+            };
+};
+
+document.querySelector('.gridSize').innerHTML = "16 X 16"
+makeGrid(16);
+
+//button function
+const inputGrid = function(){
+   
+    input = prompt ("Enter Grid Size","16");
+    if (prompt != null  ){
+        const gridAll = document.querySelectorAll ('.grid');
+        for (i=0; i < gridAll.length; i++){ 
+            gridAll[i].remove();
+        }
+        document.querySelector('.gridSize').innerHTML =
+        input + "x" + input;
+
+       makeGrid(input);
+    }
 }
 
 
 
+
+// make default grid change to new grid
+// reset grid everytime input update
